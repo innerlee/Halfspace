b314af47c846bf3dfab106b5350963f71f037550
lizz <lizz@pku.edu.cn>
2016-02-26 00:28:05 +0800

    generate data.

--- src/common.jl
+++ src/common.jl
@@ -31,8 +31,33 @@ function load_data()
   say("load data not implemented yet")
 end
 
-""" generate synthetic data """
-function generate_data()
+""" generate synthetic data, column vectors """
+function generate_data(;name=timestr(), train=25000, validation=5000, test=20000, d=50, p=2, prob=0.9)
   isdir("data") || mkdir("data")
-  say("generate data not implemented yet")
+  
+  n = train + validation + test
+  data = rand([-1, 1], d + 1, n)
+  data[end, :] = 1
+  
+  p_index = sample(1:d, p, replace=false, ordered=true)
+  labels = prod(data[p_index, :], 1)[:]
+  
+  mutate_index = sample(1:n, floor(Int, (1-prob)*n), replace=false, ordered=true)
+  mutate = ones(Int, n)
+  mutate[mutate_index] = -1
+  labels = labels .* mutate
+  
+  train_index = 1 : train
+  validation_index = train + 1 : train + validation
+  test_index = train + validation + 1 : n
+  
+  save("data/$name.jld",  "train_data", data[:, train_index], 
+                          "validation_data", data[:, validation_index],
+                          "test_data", data[:, test_index],
+                          "train_label", labels[train_index],
+                          "validation_label", labels[validation_index],
+                          "test_label", labels[test_index],
+                          "p_index", p_index)
+  
+  say("data generated in `data/$name.jld`")
 end
