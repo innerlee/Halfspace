87b5957407e0ae45890a08a108c806c148dd0ef3
innerlee <363664470@qq.com>
2016-03-01 11:05:25 +0800

    accelerate by SubArray

--- src/alg4.jl
+++ src/alg4.jl
@@ -26,7 +26,7 @@ function boostNet(features, labels; m=2, delta=.5, gamma=1, T=10, B=10)
   d, n = size(features) # feature dim
   size(labels) == (1, n) || error("boostNet: column number of features should equal to number of labels")  
 
-  net = Net2(zeros(T), zeros(T, d))  # init f0 = 0, b0 = 0
+  net = Net2(1, zeros(T), zeros(T, d))  # init f0 = 0, b0 = 0
   
   for t = 1:T
     alpha = normalize1(exp(-labels .* sigma(evaluate(net, features))))
@@ -37,10 +37,11 @@ function boostNet(features, labels; m=2, delta=.5, gamma=1, T=10, B=10)
     
     assert(size(w) == (1, d))
     
-    G = sum(alpha .* sigma(-labels .* (w * features)))
+    G = sum(alpha .* sigma(-labels .* sigma(w * features)))
     
     mu = max(-.5, G)
     b = .5 * log((1 - mu) / (1 + mu))
+    net.t = t
     net.b[t] = b
     net.W[t, :] = w
   end
@@ -56,13 +57,13 @@ sigma = tanh
 
 " two layer boost net model "
 type Net2
-  b     # a column vector
-  W    # w matrix, each row is a w
+  t   # how many available
+  b   # a column vector
+  W   # w matrix, each row is a w
 end
 
 " output value "
-evaluate(m::Net2, features) = sum(sigma(m.W * features) .* m.b, 1)
-#evaluate(m::Net2, features) = sum((m.W * features) .* m.b, 1)
+evaluate(m::Net2, features) = sum(sigma(slice(m.W, 1:m.t, :) * features) .* m.b[1:m.t], 1)
 
 " output +-1 "
 predict(m::Net2, features) = (evaluate(m, features) .> 0) * 2 - 1
--- test/go.jl
+++ test/go.jl
@@ -4,8 +4,9 @@ dataset = "set5" # "set2", "set5"
 train_data, train_label, validation_data, validation_label, test_data, test_label =
   share(load_data(dataset))
   
-model = boostNet(train_data, train_label', m=2, T=100, B=10)
+model = boostNet(train_data, train_label', m=2, T=1000, B=10)
 
+say()
 p = predict(model, train_data)
 say("train error: ", mean(p .!= train_label'))
 
